446594e8b3d5970118775fdc1d30363de4feadaa
zak90847 <zs251@kent.ac.uk>
2026-02-05 12:39:57 +0000

    bug 6

--- old_system.py
+++ old_system.py
@@ -57,7 +57,7 @@ def run_system_monolith():
             count = 0
             
             for rank in r:
-                if rank == "Captain" or "Commander": 
+                if rank == "Captain" or rank == "Commander": 
                     count = count + 1
             print("High ranking officers: " + count) 
             
