bd0e73897b77902f85b2beec58d07a70fccb1adb
zak90847 <zs251@kent.ac.uk>
2026-02-05 12:26:36 +0000

    bug 1

--- old_system.py
+++ old_system.py
@@ -1,6 +1,6 @@
 n = ["Picard", "Riker", "Data", "Worf"]
 r = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
-d = ["Command", "Command", "Operations", "Security"]
+d = ["Command", "Command", "Operations", "Security"] 
 
 active = True
 
@@ -13,7 +13,7 @@ def run_system_monolith():
     loading = 0
     while loading < 5:
         print("Loading module " + str(loading))
-        
+        loading += 1
     
     while True:
         print("\n--- MENU ---")
